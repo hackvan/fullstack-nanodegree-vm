90810d4f4359106954cb5d624d4e95af9c489058
Diego Camacho <hackvan@gmail.com>
2016-07-08 22:04:06 -0500

    Adding SQL commands to create tables on tournament database

---
+++ vagrant/01_subqueries_quiz.py
@@ -0,0 +1,16 @@
+# Find the players whose weight is less than the average.
+# 
+# The function below performs two database queries in order to find the right players.
+# Refactor this code so that it performs only one query.
+#
+
+def lightweights(cursor):
+    """Returns a list of the players in the db whose weight is less than the average."""
+    #cursor.execute("select avg(weight) as av from players;")
+    #av = cursor.fetchall()[0][0]  # first column of first (and only) row
+    #cursor.execute("select name, weight from players where weight < " + str(av))
+	
+	#cursor.execute("select name, weight from players, (select avg(weight) as av from players) as subq where players.weight < subq.avg;")
+    
+	cursor.execute("select name, weight from players where weight < (select avg(weight) as av from players);")
+    return cursor.fetchall()
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,24 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-CREATE DATABASE tournament;
+-- DROP IF EXISTS CREATE DATABASE tournament;
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS players;
+CREATE TABLE players (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	total_rounds INTEGER DEFAULT 0,
+	total_wins   INTEGER DEFAULT 0,
+	total_draws  INTEGER DEFAULT 0,
+	total_loses  INTEGER DEFAULT 0,
+	points       INTEGER DEFAULT 0
+);
 
+CREATE TABLE matches (
+	round SERIAL PRIMARY KEY,
+	id_player1 INTEGER REFERENCES players(id),
+	id_player2 INTEGER REFERENCES players(id),
+	id_winner  INTEGER REFERENCES players(id),
+	id_loser   INTEGER REFERENCES players(id),
+	is_draw    BOOLEAN DEFAULT FALSE
+);
